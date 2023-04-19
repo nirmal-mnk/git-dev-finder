f2aec2efea767b555d287e5627c3d83b52a50bfd
Nirmal <nirmalkumarm46@gmail.com>
2023-04-19 17:09:15 +0530

    added github link to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p>Created by Nirmal</p>
+      <p>
+        Created by <a href="https://github.com/nirmal-mnk"></a>Nirmal
+      </p>
     </div>
   );
 }
